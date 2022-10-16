e88e9f063ee90b7142808830f05f4fe36c8dfce8
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-10-16 16:30:54 -0400

    Add link to interview prep page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,10 +1,14 @@
+import {Link} from 'react-router-dom';
+
 function Home() {
   return (
     <>
       <p className="text-3xl font-bold underline mb-10"> Home page </p>
-      <button className="bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        My preparation for technical interviews
-      </button>
+      <Link to="/interview-prep">
+        <button className="bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+          My preparation for technical interviews
+        </button>
+      </Link>
     </>
   );
 }
