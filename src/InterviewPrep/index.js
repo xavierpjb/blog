3f722971bd6003c043a051152d5607802f2cb109
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-10-16 18:14:03 -0400

    Add introduction

--- src/InterviewPrep/index.js
+++ src/InterviewPrep/index.js
@@ -1,6 +1,57 @@
 function InterviewPrep() {
   return (
-    <p> Interview prep page</p>
+    <div>
+      <h4> TLDR </h4>
+      <p>
+        This is a summary of the things I did during my interview and
+        study process throughout my preperation. The gist of this
+        is to start prepping months ahead of time to allow yourself to learn
+        each topic for algo/ds well enough to solve unseen leetcode easys in
+        under 15 mins, mediums in 20-30 mins and hards in under 45 mins.
+      </p>
+
+      <h4>Introduction</h4>
+      <h3>Background</h3>
+      <p>
+        I Studied at UMass Dartmouth (NOT the ivy league school) and graduated
+        at 20 in 2019. From 2019 to 2020 I worked at
+        <a href="https://www.dell.com/en-us/blog/tags/dell-emc/">Dell EMC</a>,
+        where I spent a year doing front end development.
+
+        The following year, me and a good friend of mine built a startup, Mvral
+        (pronouced mural) which we stopped after a year since we couldn't figure
+        out how to generate revenue of the app we made.
+
+        After telling myself I would return to grad school, I figured I might as
+        well have spend my extra time studying for coding interviews, since if
+        you're trying to work at most of these impactful and well paying places,
+        you'll need that. Ended up not going because I got offers.
+      </p>
+
+      <h3>Duration of Studies</h3>
+      <p>
+        I started intensely studying for algo/ds mid July 2021 and practiced
+        leetcoding and mock interviews until the (virtual) onsite interviews,
+        which occured mid April 2022 <b>(9 MONTHS OF PREPARATION)</b>.
+
+        This time varies widely from person to person. I've read some people
+        who preped for 2 weeks, many mention 3 months to be sufficient, while
+        other have spent years trying to crack it, so don't feel bad if it seems
+        to be taking long to solve these sorts of problems.
+
+        Moreover, this prepartion was solely for algo/ds. I didn't do much
+        system design since I wouldn't get interviewed on it, but I would assume
+        that an extra 2 to 6 months of prep would be added if I had to study that
+        as well.
+      </p>
+
+      <h3>Offers</h3>
+      <p>
+        I was able to get an Amazon internship and return offer (SDE1 ~200k),
+        and a Google offer (L3 ~230k), both in nyc.
+      </p>
+
+    </div>
   );
 }
 
