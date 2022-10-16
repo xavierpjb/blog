d69e640192849ad7f5614d7a58f57668c428aea1
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-10-16 16:17:20 -0400

    Add react router

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <p className="text-3xl font-bold underline mb-10"> Home page </p>
-      <button class="bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+      <button className="bg-transparent hover:bg-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         My preparation for technical interviews
       </button>
     </>
