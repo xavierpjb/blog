73fa1a85c0d59920e8aedd7cffd831a5cb604531
xapatjb4 <31086996+xapatjb4@users.noreply.github.com>
2022-10-23 19:35:41 -0400

    Replace class with classname

--- src/InterviewPrep/index.js
+++ src/InterviewPrep/index.js
@@ -1,9 +1,9 @@
 function InterviewPrep() {
   return (
-    <div class="flex h-screen">
-      <div class="m-auto max-w-4xl p-5">
-        <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600"> TLDR </h2>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+    <div className="flex h-screen">
+      <div className="m-auto max-w-4xl p-5">
+        <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600"> TLDR </h2>
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           This is a summary of the things I did during my interview and
           study process throughout my preperation. The gist of this
           is to start prepping months ahead of time to allow yourself to learn
@@ -11,9 +11,9 @@ function InterviewPrep() {
           under 15 mins, mediums in 20-30 mins and hards in under 45 mins.
         </p>
 
-        <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">Introduction</h2>
-        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Background</h3>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">Introduction</h2>
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Background</h3>
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           I Studied at UMass Dartmouth (NOT the ivy league school) and graduated
           at 20 in 2019. From 2019 to 2020 I worked at
           <a href="https://www.dell.com/en-us/blog/tags/dell-emc/">Dell EMC</a>,
@@ -29,8 +29,8 @@ function InterviewPrep() {
           you'll need that. Ended up not going because I got offers.
         </p>
 
-        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Duration of Studies</h3>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Duration of Studies</h3>
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           I started intensely studying for algo/ds mid July 2021 and practiced
           leetcoding and mock interviews until the (virtual) onsite interviews,
           which occured mid April 2022 <b>(9 MONTHS OF PREPARATION)</b>.
@@ -46,93 +46,93 @@ function InterviewPrep() {
           as well.
         </p>
 
-        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Offers</h3>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">Offers</h3>
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           I was able to get an Amazon internship and return offer (SDE1 ~200k),
           and a Google offer (L3 ~230k), both in nyc.
         </p>
 
-        <h2 class="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">Preparation</h2>
-        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400"> Learning algorithms and data structures</h3>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <h2 className="font-medium leading-tight text-4xl mt-0 mb-2 text-blue-600">Preparation</h2>
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400"> Learning algorithms and data structures</h3>
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           The most essential part of your interview process is to learn about
           algorithms and data structures. My studies consisted of:
         </p>
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.youtube.com/watch?v=HtSuA80QTyo&list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb">
             MIT 2011 YouTube lectures
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           This playlist of lectures is a good introduction to a broad set of
           algorithms and data structures. They go over time complexities and proofs
           behind different algorithms. I didn't focus to much on the proofs, but
           understanding how/why algorithms works is essential as this will help
           you in reproducing them during an interview.
         </p>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           I recommend going through these lectures and taking notes on the different
           topics. It will help when refreshing your memory on what the lectures
           talked about.
         </p>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.amazon.com/Introduction-Algorithms-3rd-MIT-Press/dp/0262033844">
             Introduction to Algorithms by CLRS
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           This book was used along side the lectures when I wanted to get written
           information of the algorithms discussed, but also contained good
           exercises to try for what was discussed.
         </p>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.youtube.com/watch?v=ZA-tUyM_y7s&list=PLUl4u3cNGP63EdVPNLG3ToM6LaEUuStEY">
             MIT 2020 Youtube lectures
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           Though you may not need to go through this second set of lectures, I
           found that having this information in a second form made it easier
           to learn since I breezed through the parts I had a solid understanding
           of from the first lecture, and took my time on what I lacked.
         </p>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.amazon.com/Competitive-Programming-3rd-Steven-Halim/dp/B00FG8MNN8">
             [OPTIONAL] Competitive Programming 3
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           This book is good in the sense that it prepares you to solve
           algorithmic problems under timed constraints, but it's overkill. Start
           using this if you find leetcode mediums too easy.
         </p>
 
-        <h3 class="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">
+        <h3 className="font-medium leading-tight text-3xl mt-0 mb-2 text-blue-400">
           Learning how to Leetcode
         </h3>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850">
             Cracking the coding interview
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           The OG of how to take coding interviews. Reading everything, up to the
           exercises, is useful in setting up what to expect in a coding interiew.
           it's a bit outdated now because coding interviews are no longer don't at
           whiteboards. Replace anything with a white by a text editor and you should
           be able to use this book.
         </p>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           The most useful part of this book (or what I found myself looking at
           constantly) was the table which consists of the different topics to
           study
         </p>
         <div
-          class="flex justify-center m-5"
+          className="flex justify-center m-5"
         >
           <img
             src="https://miro.medium.com/max/954/1*cATM9kYhtwResSoDkwlP7A.png"
@@ -140,12 +140,12 @@ function InterviewPrep() {
           />
         </div>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.teamblind.com/post/New-Year-Gift---Curated-List-of-Top-75-LeetCode-Questions-to-Save-Your-Time-OaM1orEU">
             Blind 75
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           The GOD SENT leetcode guide. This is the gold standard of topics and
           problems to solve and understand on leetcode. Use these questions
           under timed constraints when you want to simulate a real interview.
@@ -154,21 +154,21 @@ function InterviewPrep() {
           understand the idea or pattern applied to solve it.
         </p>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.youtube.com/watch?v=iL6Iphg77DA&list=PLTh5zOK8tL23zhRRD5gFvFQ69D7hUqiAs">
             Welcome to the coding interview: You Suck
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           This playlist goes over tips on how to approach leetcode problem
         </p>
 
-        <h4 class="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
+        <h4 className="font-medium leading-tight text-2xl mt-0 mb-2 text-blue-300">
           <a href="https://www.pramp.com/#/">
             PraMP
           </a>
         </h4>
-        <p class="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
+        <p className="text-base font-light leading-relaxed mt-0 mb-4 text-gray-800">
           Practice Makes Perfect (Pramp) is a great way to get free mock
           interviews and also take the role of the interviewer. Take these
           as an opportunity to get better at articulating your thought process
